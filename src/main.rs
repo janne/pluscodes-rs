927a342e1b2b71ecf3c842638e5a07171cb67e65
Jan Andersson <jan.andersson@gmail.com>
2022-11-09 15:46:19 +0100

    Add encoding to main

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,11 @@ fn usage() {
 
 fn main() {
     usage();
+    let coord = pluscodes::Coordinate {
+        latitude: 59.332438,
+        longitude: 18.118813,
+    };
+    if let Some(code) = pluscodes::encode(&coord, 10) {
+        println!("{}", code);
+    }
 }
