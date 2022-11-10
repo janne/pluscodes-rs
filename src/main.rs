d8caefa09ab6669617f036f9213ec9a7a37b4717
Jan Andersson <jan.andersson@gmail.com>
2022-11-10 08:05:23 +0100

    Basic CLI functionality

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,43 @@
-fn usage() {
-    println!("pluscodes");
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(about = "Encode and parse plus codes (https://plus.codes/)")]
+enum Opt {
+    Encode {
+        #[structopt(long, default_value = "10")]
+        length: usize,
+        latitude: String,
+        longitude: String,
+    },
+    Decode {
+        code: String,
+    },
 }
 
 fn main() {
-    usage();
-    let coord = pluscodes::Coordinate {
-        latitude: 59.332438,
-        longitude: 18.118813,
-    };
-    if let Some(code) = pluscodes::encode(&coord, 10) {
-        println!("{}", code);
+    let opt = Opt::from_args();
+
+    match opt {
+        Opt::Encode {
+            length,
+            latitude,
+            longitude,
+        } => {
+            let coord = pluscodes::Coordinate {
+                latitude: latitude.parse().unwrap(),
+                longitude: longitude.parse().unwrap(),
+            };
+            if let Some(code) = pluscodes::encode(&coord, length) {
+                println!("{}", code);
+            }
+        }
+        Opt::Decode { code } => {
+            if let Some(coords) = pluscodes::decode(&code) {
+                println!(
+                    "Latitude: {}, Longitude: {}",
+                    coords.latitude, coords.longitude
+                );
+            }
+        }
     }
 }
